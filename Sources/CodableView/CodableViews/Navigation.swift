d05ff45914068a783ed4217b2209900d1c2cef78
Danny Bolella <dbolella@gmail.com>
2021-10-08 12:17:54 -0400

    Add Navigation

---
+++ Sources/CodableView/CodableViews/Navigation.swift
@@ -0,0 +1,31 @@
+//
+//  Navigation.swift
+//  
+//
+//  Created by Daniel Bolella on 10/8/21.
+//
+
+import SwiftUI
+
+struct Navigation: View, Codable {
+    var navigationTitle: String = ""
+    var views: [CodableView]
+    
+    var body: some View {
+        NavigationView {
+            List(views) { view in
+                view
+            }
+        }
+        .navigationTitle(navigationTitle)
+    }
+}
+
+struct Navigation_Previews: PreviewProvider {
+    static var previews: some View {
+        let decoder = JSONDecoder()
+        let decodedView = try! decoder.decode(CodableView.self, from: jsonTable)
+        
+        Navigation(navigationTitle: "Hello World!", views: [decodedView])
+    }
+}
