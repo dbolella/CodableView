7db084c616e61dc918532dec918b6e746ae849ed
Danny Bolella <dbolella@gmail.com>
2021-10-08 10:25:30 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CodableView
+
+A description of this package.
---
+++ Tests/CodableViewTests/CodableViewTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CodableView
+
+final class CodableViewTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CodableView().text, "Hello, World!")
+    }
+}
